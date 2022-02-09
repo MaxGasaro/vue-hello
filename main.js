6424e55e5376a0b858c570035a9d818b2354df15
Massimiliano Gasaro <massimilianogasaro95@gmail.com>
2022-02-09 16:07:27 +0100

    primo progetto vuejs

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            message: 'Hello World',
+            img: 'https://www.dellerbamarmi.it/site/wp-content/uploads/2019/06/dell-erba-progetti-apppark-g-018.jpg',
+        }
+    }
+);
